c0a332d2a5bdf6c97159187c91c2b48e79a24ba2
John Clem <johnnyclem@gmail.com>
2013-01-22 12:27:57 -0800

    commented out merge stylesheets

--- config/environments/production.js
+++ config/environments/production.js
@@ -3,8 +3,8 @@ module.exports = function (compound) {
 
     app.configure('production', function () {
         app.enable('merge javascripts');
-        app.enable('merge stylesheets');
-        app.disable('assets timestamps');
+        // app.enable('merge stylesheets');
+        // app.disable('assets timestamps');
         app.use(require('express').errorHandler());
         app.settings.quiet = true;
     });
